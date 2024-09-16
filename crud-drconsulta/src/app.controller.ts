530840290236e4d76e9dcf1e15610c2bac74b8da
felipe bevi <felipe@9ideias.com>
2024-09-16 13:58:33 -0300

    criado tela simples para o endpoint / para operar o crud alem das API

--- crud-drconsulta/src/app.controller.ts
+++ crud-drconsulta/src/app.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { join } from 'path';
+import { Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +8,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHtml(@Res() res: Response) {
+    res.sendFile(join(__dirname, '..', 'public', 'index.html'));
   }
 }
