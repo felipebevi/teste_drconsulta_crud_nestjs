24ba412940477e1ed5783382600ef53a3034363c
felipe bevi <felipe@9ideias.com>
2024-09-16 10:43:19 -0300

    implementado metodo de atualizacao da entidade User com exemplo de busca assincrona (findOneAsync)

--- crud-drconsulta/src/users/users.service.ts
+++ crud-drconsulta/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Get, HttpException, Injectable, Param, Patch } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
@@ -7,28 +7,61 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+  usersService: any;
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
   ) {}
 
-  create(createUserDto: CreateUserDto) {
+  create(
+    createUserDto: CreateUserDto
+  ) {
     return this.userRepository.save(createUserDto);
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.userRepository.find();
   }
-
+  
+  // metodo padrao de busca
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.userRepository.findOneBy({ id: id });
+  }  
+
+  // ex. metodo async para tratamento de erro e melhor fluxo de processamento
+  async findOneAsync(id: number): Promise<User> {
+    const userData =
+      await this.userRepository.findOneBy({ id });
+    if (!userData) {
+      throw new HttpException(
+        'User Not Found',
+        404,
+      );
+    }
+    return userData;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  @Patch(':id')
+  async update(
+    @Param('id') 
+    id: number, 
+
+    @Body() 
+    updateUserDto: UpdateUserDto
+  ) {
+    const existingUser = await this.findOneAsync(id);
+    const userData = this.userRepository.merge(
+      existingUser,
+      updateUserDto,
+    );
+    return this.userRepository.save(
+      userData,
+    );
   }
 
-  remove(id: number) {
+  remove(
+    id: number
+  ) {
     return `This action removes a #${id} user`;
   }
 }
